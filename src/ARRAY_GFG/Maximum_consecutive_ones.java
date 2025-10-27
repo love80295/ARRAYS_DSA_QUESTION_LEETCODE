9bff473ff6de822be8d1e47d9159d06c88d6c684
loveagrawal <loveagrawal80295@gmail.com>
2025-10-27 14:50:49 +0530

    init 6

--- src/ARRAY_GFG/Maximum_consecutive_ones.java
+++ src/ARRAY_GFG/Maximum_consecutive_ones.java
@@ -3,22 +3,24 @@ import java.util.*;
 public class Maximum_consecutive_ones {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        // took the values in binery only;
         int n = sc.nextInt();
         int[]arr = new int[n];
         for(int i = 0 ; i<arr.length ; i++){
             arr[i] = sc.nextInt();
         }
         int count = 1;
-        int sub_count = 0;
+        int sub_count = 1;
         for(int i = 0 ; i<arr.length-1 ; i++){
             if(arr[i]==arr[i+1]){
                  sub_count++;
             }
             if(sub_count>count){
                 count = sub_count;
+                sub_count = 1;
             }
         }
-        if(sub_count==0){
+        if(sub_count==1 && count==1){
             System.out.println(0);
         }else{
             System.out.println(count);
